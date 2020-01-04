e383388173b99a684ab7b87179b2f6c78e1a486c
Hendi Santika <hendisantika@gmail.com>
2020-01-05 06:18:14 +0700

    #5 - Add LogLevel Enum

---
+++ src/main/java/com/hendisantika/springbootsingletonpattern/util/LogLevel.java
@@ -0,0 +1,19 @@
+package com.hendisantika.springbootsingletonpattern.util;
+
+import org.springframework.context.annotation.Description;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-singleton-pattern
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 05/01/20
+ * Time: 06.17
+ */
+@Description(value = "Potential logging levels.")
+public enum LogLevel {
+
+    INFO, ERROR, WARN
+
+}
