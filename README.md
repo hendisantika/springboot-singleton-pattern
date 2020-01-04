ed9479039ae048994a4cdf1a32b24ec2a66b835b
Hendi Santika <hendisantika@gmail.com>
2020-01-05 06:13:52 +0700

    #1 - Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# springboot-singleton-pattern-
---
+++ src/test/java/com/hendisantika/springbootsingletonpattern/SpringbootSingletonPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootsingletonpattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSingletonPatternApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
